30f25b4a52c36b8a790732aa375b9a31e61bb168
olivercolas <olivercolas.oc@gmail.com>
2020-05-25 18:19:25 +0100

    feat(component): Added title component for pages

---
+++ src/components/Title.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="title display-1 text-center">{{ text }}</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Title extends Vue {
+  @Prop(String) readonly text: string | undefined;
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+    background-color: #f2f2f2;
+    border-radius: 4px;
+    padding:5px;
+    margin-bottom: 25px;
+}
+</style>
