837f076c55da0b67567e1c1465283a39c3c63256
olivercolas <olivercolas.oc@gmail.com>
2020-05-25 18:17:25 +0100

    style(App): Added main layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,56 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <!-- <v-app-bar dense app color="secondary" dark>
+      <v-toolbar-title>Vue Dashboard</v-toolbar-title>
+    </v-app-bar> -->
+
+    <v-navigation-drawer app mini-variant permanent>
+      <v-list dense nav>
+        <v-list-item v-for="link in links" :key="link.title" :to="link.url">
+          <v-tooltip right>
+            <template v-slot:activator="{ on }">
+              <v-list-item-icon v-on="on" style="margin-left:0px;">
+                <v-icon>{{ link.icon }}</v-icon>
+              </v-list-item-icon>
+            </template>
+            <span>{{ link.title }}</span>
+          </v-tooltip>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import { Link } from '@/models/link';
 
-#nav {
-  padding: 30px;
+@Component
+export default class App extends Vue {
+  links: Link[] = [
+    {
+      icon: 'mdi-chart-bar',
+      title: 'Bar Chart',
+      url: '/',
+    },
+    {
+      icon: 'mdi-chart-pie',
+      title: 'Pie Chart',
+      url: '/pie-chart',
+    },
+    {
+      icon: 'mdi-map-marker',
+      title: 'Geographic',
+      url: '/geographic-chart',
+    },
+  ];
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  mounted() {
+    this.$store.dispatch('people/getPeople');
   }
 }
-</style>
+</script>
